7db747c5a73ca2bd6f03159983a0e2c8b858b21b
Troy Dorman <troydorman@gmail.com>
2021-05-27 08:23:35 -0600

    removed default image drag event

---
+++ script.js
@@ -0,0 +1,19 @@
+const slider = document.querySelector('.slider-container'),
+slides = Array.from(document.querySelectorAll('.slide'));
+
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0,
+    currentIndex = 0;
+
+slides.forEach((slide, index) => {
+    const slideImage = slide.querySelector('img');
+    slideImage.addEventListener('dragstart', (e) => e.preventDefault());   
+
+    // touch events
+    
+
+    // mouse events
+})
